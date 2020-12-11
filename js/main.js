900fec38b526616a1efbb6288014c96402f22ffc
schy-gif <schy-gif@users.noreply.github.com>
2020-12-11 08:13:26 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+    // FIX THIS . PUT IT IN ITS OWN FILE
+     
+
+
+
+$("#layout1__s3").hover(function(){
+    $("#fi-02").css({"opacity":1})
+}, function(){
+    $("#fi-02").css({"opacity":0})
+})
+
+
+    //FIX THIS . PUT IT IN ITS OWN FILE
+     
+
+
+
+$("#layout1__s1").hover(function(){
+    $(".textbox1").css({"opacity":1})
+}, function(){
+    $(".textbox1").css({"opacity":0})
+})
+
+
+$("#layout1__s2").hover(function(){
+    $(".textbox2").css({"opacity":1})
+}, function(){
+    $(".textbox2").css({"opacity":0})
+})
